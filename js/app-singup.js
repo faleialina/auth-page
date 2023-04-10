eae4bf8adbafbc5a58796f83b59fdda75a38d7fb
faleialina <august3@mail.ru>
2023-04-10 19:39:58 +0300

    auth-page/+

--- js/app-singup.js
+++ js/app-singup.js
@@ -5,32 +5,41 @@
 // то вывести alert “Вы успешно зарегистрированы / авторизованы в системе”, в зависимости 
 // от страницы. Если проверки не прошли, то вывсти alert с соответствующим сообщением об ошибке
 
+class SingUp {
+    constructor() {
 
-const btn = document.querySelector('button');
-const inp1 = document.querySelector('.inp-mail');
-const inp2 = document.querySelector('.inp-pwd');
-const inp3 = document.querySelector('.inp-pwd-conf');
+        this.doRegistration()
 
-
-function isValid(mail, pwd, pwdConf) {
-    if (mail === '' || pwd === '') throw new Error('Пустая строка')
-    if (!/^[a-z0-9_\.-]+@[a-z]+\.[a-z]{2,3}$/gm.test(mail) && !/^\+[1-9]{12}$/gm.test(mail)) throw new Error('некорректный ввод почты / номера телефона');
-    if (!/^[\w\+\!\.\@\#\$\%\&\*\(\)\/\\]{8,}$/gm.test(pwd)) throw new Error('Пароль не соответствует требованиям безопасности');
-    if (pwdConf !== pwd) throw new Error('Пароль не подтвержден');
-}
-
-btn.addEventListener('click', function () {
-    try {
-        let mail = inp1.value;
-        let pwd = inp2.value;
-        let pwdConf = inp3.value;
-        isValid(mail, pwd, pwdConf)
-        alert('Вы успешно зарегистрированы в системе');
-        inp1.value = '';
-        inp2.value = '';
-        inp3.value = '';
-    } catch (error) {
-        alert(error.message)
     }
 
-})
+    isValid(mail, pwd, pwdConf) {
+        if (mail === '' || pwd === '') throw new Error('Пустая строка')
+        if (!/^[a-z0-9_\.-]+@[a-z]+\.[a-z]{2,3}$/gm.test(mail) && !/^\+[1-9]{12}$/gm.test(mail)) throw new Error('некорректный ввод почты / номера телефона');
+        if (!/^[\w\+\!\.\@\#\$\%\&\*\(\)\/\\]{8,}$/gm.test(pwd)) throw new Error('Пароль не соответствует требованиям безопасности');
+        if (pwdConf !== pwd) throw new Error('Пароль не подтвержден');
+    }
+    doRegistration() {
+        const btn = document.querySelector('button');
+        const inp1 = document.querySelector('.inp-mail');
+        const inp2 = document.querySelector('.inp-pwd');
+        const inp3 = document.querySelector('.inp-pwd-conf');
+
+
+        btn.addEventListener('click', () => {
+            try {
+                let mail = inp1.value;
+                let pwd = inp2.value;
+                let pwdConf = inp3.value;
+                this.isValid(mail, pwd, pwdConf)
+                alert('Вы успешно зарегистрированы в системе');
+                inp1.value = '';
+                inp2.value = '';
+                inp3.value = '';
+            } catch (error) {
+                alert(error.message)
+            }
+
+        })
+    }
+}
+const singUp = new SingUp();
